c5fe1f25d2025d40e23aef2bd13fdf3bb691c90e
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2022-06-15 23:03:43 +0500

    Assignment 3

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,15 +1,5 @@
 import React, { Component } from "react";
-import {
-	Card,
-	CardImg,
-	CardImgOverlay,
-	CardTitle,
-	Breadcrumb,
-	BreadcrumbItem,
-	CardBody,
-	CardText,
-} from "reactstrap";
-import { Link } from "react-router-dom";
+
 import {
 	Button,
 	Modal,
@@ -56,6 +46,7 @@ class CommentForm extends Component {
 					<span className='fa fa-comments fa-lg'></span> Submit Comment
 				</Button>
 
+				{/* commentform  Modal */}
 				<Modal
 					isOpen={this.state.isCommentFormModalOpen}
 					toggle={this.toggleCommentFormModal}
@@ -172,81 +163,4 @@ class CommentForm extends Component {
 	}
 }
 
-function RenderDish({ dish }) {
-	if (dish != null) {
-		return (
-			<div className='col-12 col-md-5 m-1'>
-				<Card>
-					<CardImg width='100%' src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle> {dish.name}</CardTitle>
-						<CardText> {dish.description} </CardText>
-					</CardBody>
-				</Card>
-			</div>
-		);
-	} else {
-		return <div></div>;
-	}
-}
-
-function RenderComments({ dish, comments }) {
-	if (comments == null) {
-		return <div></div>;
-	}
-	const cmnts = comments.map((comment) => {
-		return (
-			<li key={comment.id}>
-				<p>{comment.comment}</p>
-				<p>
-					-- {comment.author}, &nbsp;
-					{new Intl.DateTimeFormat("en-US", {
-						year: "numeric",
-						month: "long",
-						day: "2-digit",
-					}).format(new Date(comment.date))}
-				</p>
-			</li>
-		);
-	});
-	return (
-		<div className='col-12 col-md-5 m-1'>
-			<h4> Comments </h4>
-			<ul className='list-unstyled'>{cmnts}</ul>
-			<CommentForm dish={dish} comments={comments} />
-		</div>
-	);
-}
-
-const DishDetail = (props) => {
-	const dish = props.dish;
-
-	if (dish == null) {
-		return <div></div>;
-	}
-
-	return (
-		<div className='container'>
-			<div className='row'>
-				<Breadcrumb>
-					<BreadcrumbItem>
-						<Link to='/menu'>Menu</Link>
-					</BreadcrumbItem>
-					<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-				</Breadcrumb>
-
-				<div className='col-12'>
-					<h3> {props.dish.menu}</h3>
-					<hr />
-				</div>
-			</div>
-
-			<div className='row'>
-				<RenderDish dish={props.dish} />
-				<RenderComments dish={props.dish} comments={props.comments} />
-			</div>
-		</div>
-	);
-};
-
-export default DishDetail;
+export default CommentForm;
